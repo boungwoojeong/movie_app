dfead090691302bfafe77ccda9f4db2aa9d56098
Woo Jeong <boungwoojeong@gmail.com>
2021-10-20 15:46:43 +0900

    화면 스타일 개선 및 최종 목록 완성

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 
 class App extends React.Component {
@@ -13,7 +14,7 @@ class App extends React.Component {
   getMovies = async () => {
     const movies = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
     this.setState({ movies: movies.data.data.movies, isLoading: false })
-    console.log(this.state.movies);
+    //console.log(this.state.movies);
   };
 
   componentDidMount() {
@@ -22,16 +23,33 @@ class App extends React.Component {
 
   render() {
     const { isLoading, movies } = this.state;
-    return <div>{isLoading ? "Loading..." : movies.map(movie => {
-      console.log(movie);
-      return <Movie
-        key={movie.id}
-        id={movie.id}
-        year={movie.year}
-        title={movie.title}
-        summary={movie.summary}
-        poster={movie.poster} />;
-    })}</div>;
+
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading......</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres = {movie.genres}>
+              </Movie>
+            ))}
+
+          </div>
+
+        )
+        }
+      </section>
+    );
   }
 
 }
